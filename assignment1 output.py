52f0c387b01b5257af054f7ea697a99d12bae4e9
Statrx <97702920+Statrx@users.noreply.github.com>
2022-01-14 03:16:06 +0530

    Add files via upload

---
+++ assignment1 output.py
@@ -0,0 +1,25 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====== RESTART: C:/Users/sajja/Desktop/Uni stuff/Computing/assignment 1.py =====
+Enter first number: 51
+Enter second number 75
+Enter third number 95
+The average of the three numbers is 73.66666666666667
+Enter your total income:70000
+Enter your non-earning family members:3
+Income tax to be paid is  10200.0
+Enter your SID-21105090
+Enter your name-Sajjal Rana
+Enter your Gender-(M,F,U) -  M
+Enter the course name-Introduction to Computing
+Enter the CGPA-9
+[21105090, 'Sajjal Rana', 'M', 'Introduction to Computing', 9.0]
+Enter your marks(Student 1): 78
+Enter your marks(Student 2): 45
+Enter your marks(Student 3): 89
+Enter your marks(Student 4): 95
+Enter your marks(Student 5): 67
+[45.0, 67.0, 78.0, 89.0, 95.0]
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
